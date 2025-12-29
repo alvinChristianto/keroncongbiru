d9b8972a5fc34803fe38edf2de686c32a072faca
alvinChristianto <alvinchristianto93@gmail.com>
2025-12-29 22:33:18 +0700

    feat: updating astro icon for footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from '@astrojs/sitemap';
+import icon from "astro-icon";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [tailwind(), mdx(), sitemap(), icon()],
   site: "https://keroncongbiru.vercel.app"
 });
